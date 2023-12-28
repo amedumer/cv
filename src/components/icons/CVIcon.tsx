2c1569be96bf97868d5f715577c195fe078365dc
amedumer <ahmetomer7@gmail.com>
2023-12-28 20:58:23 +0300

    feat: initial commit

---
+++ src/components/icons/CVIcon.tsx
@@ -0,0 +1,14 @@
+export const CVIcon = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <title>CV</title>
+        <path
+          fill="currentColor"
+          d="M17,1H7C5.9,1,5,1.9,5,3v18c0,1.1,0.9,2,2,2h10c1.1,0,2-0.9,2-2V3C19,1.9,18.1,1,17,1z M12,19c-0.55,0-1-0.45-1-1
+          s0.45-1,1-1s1,0.45,1,1S12.55,19,12,19z M15,15H9c-0.55,0-1-0.45-1-1s0.45-1,1-1h6c0.55,0,1,0.45,1,1S15.55,15,15,15z M15,11H9
+          C8.45,11,8,10.55,8,10s0.45-1,1-1h6c0.55,0,1,0.45,1,1S15.55,11,15,11z"
+        />
+      </svg>
+    );
+  };
+  
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -18,261 +18,163 @@ import Minimal from "../images/logos/minimal.svg";
 import { GitHubIcon } from "@/components/icons/GitHubIcon";
 import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { XIcon } from "@/components/icons/XIcon";
+import { CVIcon } from "@/components/icons/CVIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Omer Kayabasi",
+  initials: "OK",
+  location: "London, UK",
+  locationLink: "https://www.google.com/maps/place/London",
   about:
     "Full Stack Engineer focused on building products with extra attention to details",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As an innovative Full Stack Developer with a knack for blockchain and cross-platform applications, I've successfully developed and managed high-impact projects. With over 3 years of experience, my expertise spans a vast array of technologies including JavaScript, TypeScript, Python, Cosmos SDK and Flutter. I excel in leading and collaborating within diverse teams, consistently pushing the boundaries of software development to deliver exceptional, user-centric solutions.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/69629296?v=4",
+  personalWebsiteUrl: "https://ahmetomer.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "omer.kayabasi23@imperial.ac.uk",
+    tel: "+0",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/amedumer",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/ahmetomerkayabasi/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/amedumer_",
         icon: XIcon,
+      },
+      {
+        name: "CV",
+        url: "https://drive.google.com/file/d/1gaH_Q5T9auN-eGx9pZWMecbMIK_7UUR4/view",
+        icon: CVIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Imperial College London",
+      degree: "Master's Degree in Software Engineering",
+      start: "2023",
+      end: "2024",
+    },
+    {
+      school: "Sabanci University",
+      degree: "Bachelor's Degree in Computer Engineering",
+      start: "2019",
+      end: "2023",
+    },
+    {
+      school: "Sabanci University",
+      degree: "Associate's Degree in Decision and Behavioral Sciences",
+      start: "2019",
+      end: "2023",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Millicent Labs",
+      link: "https://www.millicent.io",
+      badges: ["London"],
+      title: "Full Stack Developer",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "2022",
+      end: "Now",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Led blockchain development, implemented cryptocurrency payment applications, and contributed to various software projects. Technologies: Java, Python, React, Blockchain",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Sabanci University",
+      link: "https://sabanciuniv.edu",
+      badges: ["Turkey"],
+      title: "Learning & Teaching Assistant",
+      logo: ClevertechLogo,
+      start: "2020",
+      end: "2022",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
+        "Assisted in teaching computer science courses, engaged in research projects including a cryptographic suite for IoT devices. Technologies: C++, Python, Cryptography",
+    }
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
+    "Javascript",
+    "Typescript",
+    "Python",
+    "Dart",
+    "Go",
+    "C++",
+    "C#",
+    "Rust",
+    "SQL",
+    "Solidity",
+    "HTML",
+    "Django",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
-  ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
+    "Mux",
+    "FastAPI",
+    "React.js",
+    "Next.js",
+    "SvelteKit",
+    "Bootstrap",
+    "Tailwind CSS",
+    "MUI",
+    "Flutter",
+    "Swift",
+    "Cosmos SDK",
+    "Firebase",
+    "AWS",
+    "Express",
+    "MongoDB",
+    "MySQL",
+    "Selenium",
+    "Nginx"
+],
 
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
+projects: [
+  {
+    title: "UKVisaTimeline",
+    techStack: ["SvelteKit", "TypeScript", "Pico.css", "Pocketbase", "Vercel"],
+    description: "A platform simplifying visa application processes through timeline tracking and insights, with a responsive and user-friendly interface.",
+    logo: ClevertechLogo,
+    link: {
+      label: "ukvisatimeline.com",
+      href: "https://ukvisatimeline.com/",
     },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
+  },
+  {
+    title: "Nakit",
+    techStack: ["Cosmos SDK", "IBC", "Flutter", "TEE", "SE", "QR codes", "NFC", "Bluetooth"],
+    description: "Decentralized blockchain for offline transactions with Zero-Knowledge Proofs and a Flutter-based mobile app for transaction verification and offline authorization.",
+    logo: ClevertechLogo,
+    link: {
+      label: "nakit.dev",
+      href: "",
     },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
+  },
+  {
+    title: "Cekmece",
+    techStack: ["React.js", "Next.js", "Node.js", "Flutter", "mySQL", "Scrum"],
+    description: "E-Commerce solution developed with a 5-member team, using a range of technologies and Scrum methodologies for iterative development.",
+    logo: ClevertechLogo,
+    link: {
+      label: "cekmece.com",
+      href: "",
     },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
+  },
+  {
+    title: "suEmpty",
+    techStack: ["Flutter"],
+    description: "Mobile application for finding empty classrooms at Sabanci University, featuring customizable filters for day, time, and faculty.",
+    logo: ClevertechLogo,
+    link: {
+      label: "suempty.com",
+      href: "",
     },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
+  }
+],
 } as const;
